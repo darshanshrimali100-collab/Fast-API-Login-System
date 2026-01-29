5336fbaddee0c26256734e3747ee158f6ca98cfc
Akhilesh Airen <akhilesh@summence.com>
2026-01-29 09:57:57 +0530

    update environment variables and gitignore

--- app/CONFIG/config.py
+++ app/CONFIG/config.py
@@ -8,14 +8,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 master_db = str(BASE_DIR / "database.sqlite3")
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent  # config.py -> app/ -> parent
-env_path = r'D:\DARSHAN SHRIMALI\JOB\SUMMENCE TECHNOLOGIES\FAST API LOGIN (JWT)\SOURCE CODE\API SOURCE_CODE\config.env'
-load_dotenv(dotenv_path=env_path)
-
-print("env path = ", env_path)
+load_dotenv()
 
 # JWT & cookie settings
-SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+SECRET_KEY = os.getenv("SECRET_KEY")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 600))
 SECURE_COOKIES = os.getenv("SECURE_COOKIES", "True").lower() in ["true", "1", "yes"]
 MAX_ATTEMPTS = int(os.getenv("MAX_ATTEMPTS", "5"))
 LOCK_TIME_MINUTES = int(os.getenv("LOCK_TIME_MINUTES", "1"))
@@ -23,13 +20,9 @@ TEST_MODE = os.getenv("TEST", "false").lower() == "true"
 CORS_URL = os.getenv("CORS_URL")
 SMTP_MAIL = os.getenv("SMTP_MAIL")
 SMTP_PWD = os.getenv("SMTP_PWD")
-DB_PATH = os.getenv("DB_PATH")
-
-# Used In Models router
-DATA_FOLDER = os.path.join(os.getcwd(), "app", "DATA")
-os.makedirs(DATA_FOLDER, exist_ok=True)
+master_db = os.getenv("DB_PATH")
+DATA_FOLDER = os.getenv("DATA_FOLDER")
 
 print("MAX_ATTEMPTS =", MAX_ATTEMPTS)
 print("LOCK_TIME_MINUTES =", LOCK_TIME_MINUTES)
 print("reading config file")
-print(DB_PATH)
