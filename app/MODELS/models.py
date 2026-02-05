0419e1e3d7aa58e90fc74446934483508bfe57ef
Darshan Shrimali <darshanshrimali100@gmail.com>
2026-02-05 11:12:41 +0530

    Add files via upload

---
+++ app/MODELS/models.py
@@ -0,0 +1,151 @@
+from pydantic import BaseModel,field_validator
+from typing import List, Dict
+from fastapi import Form
+
+
+class AddModelRequest(BaseModel):
+    model_name: str
+    model_template: str
+    project_name: str
+    upload_model_with_sample_data: bool
+
+    @field_validator(
+        "model_name",
+        "model_template",
+        "project_name"
+    )
+    @classmethod
+    def non_empty_strings(cls, v: str) -> str:
+        if not v or not v.strip():
+            raise ValueError("Model name and project name and model template are required")
+        return v.strip()
+
+class AssignModelsRequest(BaseModel):
+    target_project: str
+    models_by_project: Dict[str, List[str]]
+
+    @field_validator("target_project")
+    @classmethod
+    def validate_target_project(cls, v: str) -> str:
+        if not v.strip():
+            raise ValueError("Current project name is required")
+        return v
+
+    @field_validator("models_by_project")
+    @classmethod
+    def validate_models_by_project(cls, v: dict) -> dict:
+        if not v:
+            raise ValueError("No models provided")
+        return v
+
+class SaveAsModelRequest(BaseModel):
+    current_project_name: str
+    existing_model_name: str
+    new_model_name: str
+    project_name: str
+
+    @field_validator(
+        "current_project_name",
+        "existing_model_name",
+        "new_model_name",
+        "project_name"
+    )
+    @classmethod
+    def non_empty_strings(cls, v: str) -> str:
+        if not v or not v.strip():
+            raise ValueError("Model name and project name are required")
+        return v.strip()
+
+
+class RenameModelRequest(BaseModel):
+    current_project_name: str
+    current_model_name: str
+    new_model_name: str
+
+    @field_validator(
+        "current_project_name",
+        "current_model_name",
+        "new_model_name"
+    )
+    @classmethod
+    def non_empty_strings(cls, v: str) -> str:
+        if not v or not v.strip():
+            raise ValueError("Model name and project name are required")
+        return v.strip()
+
+
+class DeleteModelRequest(BaseModel):
+    current_project_name: str
+    model_name: str
+    project_name: str
+
+    @field_validator(
+        "current_project_name",
+        "model_name",
+        "project_name"
+    )
+    @classmethod
+    def non_empty_strings(cls, v: str) -> str:
+        if not v or not v.strip():
+            raise ValueError("current project name , Model name and project name are required")
+        return v.strip()
+
+class MoveModelToProjectRequest(BaseModel):
+    current_project_name: str
+    model_name: str
+    project_name: str
+
+    @field_validator(
+        "current_project_name",
+        "model_name",
+        "project_name"
+    )
+    @classmethod
+    def non_empty_strings(cls, v: str) -> str:
+        if not v or not v.strip():
+            raise ValueError("current project name , Model name and project name are required")
+        return v.strip()
+
+class DownloadModelRequest(BaseModel):
+    current_project_name: str
+    model_name: str  
+    project_name: str
+
+    @field_validator(
+        "current_project_name",
+        "model_name",
+        "project_name"
+    )
+    @classmethod
+    def non_empty_strings(cls, v: str) -> str:
+        if not v or not v.strip():
+            raise ValueError("current project name , Model name and project name are required")
+        return v.strip()
+
+class UploadModelPayload(BaseModel):
+    current_project_name: str
+    model_name: str 
+    project_name: str 
+
+    @field_validator(
+        "current_project_name",
+        "model_name",
+        "project_name"
+    )
+    @classmethod
+    def non_empty_strings(cls, v: str) -> str:
+        if not v or not v.strip():
+            raise ValueError("current project name , Model name and project name are required")
+        return v.strip()
+
+
+def upload_payload(
+    current_project_name: str = Form(...),
+    model_name: str = Form(...),
+    project_name: str = Form(...)
+    ):
+        return UploadModelPayload(
+            current_project_name = current_project_name,
+            model_name=model_name,
+            project_name=project_name
+        )
